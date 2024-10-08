bdf6cff976a0671aae30b2ac5f7b912ab877fdd7
maxfukuh4ra <maxfuku04@g.ucla.edu>
2024-10-07 17:20:22 -0700

    server.js static fixing for render

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,45 @@
-// setup
-require('dotenv').config()
-require('cors')
-const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
+require('dotenv').config();
+require('cors');
+const mongoose = require('mongoose');
+const express = require('express');
+const path = require('path'); // Add path to serve static files
+const app = express();
 
-//routes
-const currentSongsCollectionRoutes = require('./routes/currentSongsCollection')
-const spotifyAPIRoutes = require('./routes/spotifyAPI')
-const userRoutes = require('./routes/userRoutes')
+// Routes
+const currentSongsCollectionRoutes = require('./routes/currentSongsCollection');
+const spotifyAPIRoutes = require('./routes/spotifyAPI');
+const userRoutes = require('./routes/userRoutes');
 
-// middleware
-app.use(express.json())
+// Middleware
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    console.log(req.path, req.method);
+    next();
+});
 
-// routes
-app.use('/api/current_songs', currentSongsCollectionRoutes)
+// API Routes
+app.use('/api/current_songs', currentSongsCollectionRoutes);
+app.use('/spotify', spotifyAPIRoutes);
+app.use('/users', userRoutes);
 
-app.use('/spotify', spotifyAPIRoutes)
+// Serve static files in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/users', userRoutes)
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
-// connect to mongo database and listen for requests
+// Connect to MongoDB and listen for requests
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log('connected to db and listening on port', process.env.PORT)
-        })
+            console.log('connected to db and listening on port', process.env.PORT);
+        });
     })
     .catch((error) => {
-        console.log(error)
-    })
+        console.log(error);
+    });
